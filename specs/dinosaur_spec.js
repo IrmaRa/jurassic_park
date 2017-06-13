0318fede2e611bccc2c154fd1a95a62a59286001
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-06-13 16:46:30 +0100

    Add dinosaur constructor

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var Dinosaur = require('../dinosaur.js');
+
+describe('Dinosaur', function() {
+
+  var dinosaur;
+
+  beforeEach(function() {
+    dinosaur = new Dinosaur({type: 'Tyrannosaurus', offspring: 3});
+  });
+
+  it('should have a type', function() {
+    assert.strictEqual('Tyrannosaurus', dinosaur.type);
+  });
+
+  it('should have a offspring number', function() {
+    assert.strictEqual(3, dinosaur.offspring);
+  });
+
+
+});
